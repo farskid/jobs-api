3858f7436e7f5fdc17d8ae22d8b9529bf7df9ec1
Farzad YZ <farskid@gmail.com>
2020-11-29 14:01:17 +0200

    v1

---
+++ src/functions/api.js
@@ -0,0 +1,74 @@
+const express = require("express");
+const { default: fetch } = require("node-fetch");
+const serverlessHTTP = require("serverless-http");
+const cors = require("cors");
+const { getJobsFromHTML } = require("../jobs");
+
+const server = express();
+const apiRouter = express.Router();
+
+// const PER_PAGE = 10;
+
+apiRouter.get("/", (_, res) => {
+  res.send("Nothing on this endpoint!");
+});
+
+apiRouter.get("/jobs", (req, res) => {
+  // let { page = 1, perPage = PER_PAGE } = req.query;
+
+  // console.log("GET", `/jobs?page=${page}&perPage=${perPage}`);
+
+  const htmlReq = fetch(
+    "https://weworkremotely.com/categories/remote-programming-jobs"
+  );
+
+  htmlReq
+    .then((resp) => resp.text())
+    .then((html) => {
+      const jobs = getJobsFromHTML(html);
+
+      // const pageCount = Math.ceil(jobs.length / perPage);
+      // page = Number(page);
+
+      //   invalid page, return the first page
+      // if (!page) {
+      //   console.log(`invalid page, page is reset to 1`);
+      //   page = 1;
+      // }
+
+      // //   pages over the last page, return the last page
+      // if (page > pageCount) {
+      //   console.log(
+      //     `page over the last page, page is reset to the last page ${pageCount}`
+      //   );
+      //   page = pageCount;
+      // }
+
+      return responseDecorator(res).json({
+        // page,
+        // total: jobs.length,
+        // jobs: jobs.slice(page * perPage - perPage, page * perPage),
+        jobs,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      return res.status(500).send("Server internal err");
+    });
+});
+
+server.use(cors());
+server.use("/.netlify/functions/api", apiRouter);
+server.set("etag", false);
+
+// No idea why a middleware didn't work!
+function responseDecorator(res) {
+  // new Date().toISOString()
+  res.setHeader("x-last-modified", new Date().toISOString());
+  // Disable caching
+  res.set("Cache-Control", "no-store");
+  return res;
+}
+
+// Server will start at port 9000 by default
+module.exports.handler = serverlessHTTP(server);
---
+++ src/jobs.js
@@ -0,0 +1,27 @@
+const cheerio = require("cheerio");
+
+module.exports.getJobsFromHTML = (html) => {
+  const $ = cheerio.load(html);
+  return $(".jobs ul > li")
+    .not("#one-signal-subscription-form")
+    .map((_, el) => {
+      const isNew = !!$(el).find(".new");
+      const $job = $(el).find("a");
+      const link = $job.attr("href") || null;
+      const company = $(el).find(".company").text() || null;
+      const title = $(el).find(".title").text() || null;
+      const date = $(el).find("time").attr("datetime") || null;
+      const type = $(el).find(".company").eq(1).text() || null;
+      const region = $(el).find(".region").text() || null;
+      return {
+        isNew,
+        link: `https://weworkremotely.com${link}`,
+        company,
+        title,
+        date,
+        type,
+        region,
+      };
+    })
+    .toArray();
+};
